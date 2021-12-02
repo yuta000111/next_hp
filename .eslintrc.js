f968756dc5435af7d09e7c3d7dcacb31dff9f53f
yuta000111 <yuuta0114@gmail.com>
2021-12-02 22:00:11 +0900

    Initialize repository

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@next/next/core-web-vitals', // Next.jsとCore Web Vitalsのチェック
+        'prettier', // prettierと競合するESLintのルールを無効化します
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+    },
+};
